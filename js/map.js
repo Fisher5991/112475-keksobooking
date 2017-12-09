c118128f8e7d9131ed3fe79f534855f71076b308
Fisher5991 <fisher5991@gmail.com>
2017-12-09 21:28:45 +0300

    Личный проект: доверяй, но проверяй (предварительная версия)

--- js/map.js
+++ js/map.js
@@ -289,6 +289,7 @@ var typeOption = noticeForm.querySelector('#type');
 var priceField = noticeForm.querySelector('#price');
 var roomNumberOption = noticeForm.querySelector('#room_number');
 var capacityOption = noticeForm.querySelector('#capacity');
+var formButton = noticeForm.querySelector('.form__submit');
 var minPrices = {
   bungalo: '0',
   flat: '1000',
@@ -324,7 +325,7 @@ var removeInvalidColor = function (field) {
   field.style.border = '';
 };
 
-titleField.addEventListener('invalid', function (evt) {
+var onTitleFieldInvalid = function () {
   addInvalidColor(titleField);
   if (titleField.validity.tooShort) {
     titleField.setCustomValidity('Заголовок объявления должен состоять из не менее ' + minLengthTitle + ' символов');
@@ -336,17 +337,9 @@ titleField.addEventListener('invalid', function (evt) {
     removeInvalidColor(titleField);
     titleField.setCustomValidity('');
   }
-});
+}
 
-titleField.addEventListener('input', function (evt) {
-  if (evt.target.value.length < minLengthTitle) {
-    evt.target.setCustomValidity('Заголовок объявления должен состоять из не менее ' + minLengthTitle + ' символов');
-  } else {
-    evt.target.setCustomValidity('');
-  }
-});
-
-priceField.addEventListener('invalid', function (evt) {
+var onPriceFieldInvalid = function () {
   addInvalidColor(priceField);
   if (priceField.validity.rangeOverflow) {
     priceField.setCustomValidity('Цена не может превышать' + priceField.max);
@@ -356,9 +349,25 @@ priceField.addEventListener('invalid', function (evt) {
     removeInvalidColor(priceField);
     priceField.setCustomValidity('');
   }
-});
+};
+
+var onTitleFieldInput = function (evt) {
+  if (evt.target.value.length < minLengthTitle) {
+    evt.target.setCustomValidity('Заголовок объявления должен состоять из не менее ' + minLengthTitle + ' символов');
+  } else {
+    evt.target.setCustomValidity('');
+  }
+};
+
+var onFormButtonMouseup = function () {
+  titleField.addEventListener('invalid', onTitleFieldInvalid);
+  titleField.addEventListener('input', onTitleFieldInput);
+  priceField.addEventListener('invalid', onPriceFieldInvalid);
+}
 
 timeinOption.addEventListener('change', onTimeoutChange);
 timeoutOption.addEventListener('change', onTimeinChange);
 typeOption.addEventListener('change', onTypeChange);
 roomNumberOption.addEventListener('change', onRoomChange);
+
+formButton.addEventListener('mouseup', onFormButtonMouseup);
